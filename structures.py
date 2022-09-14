07cc8625f79feef0a13fa3ec358ccdaa7d425573
Sigizmund <40463616+samnesvoy@users.noreply.github.com>
2022-09-14 21:22:44 +0300

    Queue кольцевая

--- structures.py
+++ structures.py
@@ -18,7 +18,7 @@ class Stack:
 
     def pop(self):
         if self.__top > 0:
-            popping = self.__data.pop(self.__top-1)
+            popping = self.__data.pop(self.__top - 1)
             self.__top -= 1
             return popping
         else:
@@ -30,6 +30,7 @@ class Stack:
 
     def get_top(self):
         return self.__top
+
 
 class Queue:
     __data = list()
@@ -51,6 +52,39 @@ class Queue:
         if self.__size > 0:
             popping = self.__data.pop(0)
             self.__size -= 1
+            return popping
+        else:
+            print('пусто/empty')
+            return None
+
+    def get_capacity(self):
+        return self.__capacity
+
+    def get_size(self):
+        return self.__size
+
+
+class Queue_ring:
+    __data = list()
+    __size = 0
+    __capacity = 0
+
+    def __init__(self, capacity: int):
+        if capacity > 0:
+            self.__capacity = capacity
+
+    def add(self, element: any):
+        if self.__size < self.__capacity:
+            self.__data.append(element)
+            self.__size += 1
+        else:
+            print('переполнение/overflow')
+
+    def pop(self):
+        if self.__size > 0:
+            popping = self.__data.pop(0)
+            # выдавливаемый элемент добавляется в хвост очереди-
+            self.add(popping)
             return popping
         else:
             print('пусто/empty')

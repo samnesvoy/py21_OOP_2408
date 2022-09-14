9f54245a851bbe350f8ccebbd18f69efc4f3b331
Sigizmund <40463616+samnesvoy@users.noreply.github.com>
2022-09-14 21:09:19 +0300

    Queue

--- structures.py
+++ structures.py
@@ -30,3 +30,34 @@ class Stack:
 
     def get_top(self):
         return self.__top
+
+class Queue:
+    __data = list()
+    __size = 0
+    __capacity = 0
+
+    def __init__(self, capacity: int):
+        if capacity > 0:
+            self.__capacity = capacity
+
+    def add(self, element: any):
+        if self.__size < self.__capacity:
+            self.__data.append(element)
+            self.__size += 1
+        else:
+            print('переполнение/overflow')
+
+    def pop(self):
+        if self.__size > 0:
+            popping = self.__data.pop(0)
+            self.__size -= 1
+            return popping
+        else:
+            print('пусто/empty')
+            return None
+
+    def get_capacity(self):
+        return self.__capacity
+
+    def get_size(self):
+        return self.__size

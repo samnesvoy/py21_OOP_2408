3cfa1ad2b6ab27b5fb5265cee62949cdc77e517e
Sigizmund <40463616+samnesvoy@users.noreply.github.com>
2022-09-21 20:15:47 +0300

    threads with args in for

--- mult.py
+++ mult.py
@@ -10,21 +10,29 @@ def fun(number: int):
 
 start = time.perf_counter()
 threads=[]
-thread1 = Thread(target=fun)
-thread2 = Thread(target=fun)
-thread3 = Thread(target=fun)
-thread4 = Thread(target=fun)
-# fun()
-# fun()
-thread1.start()
-thread2.start()
-thread3.start()
-thread4.start()
+# thread1 = Thread(target=fun)
+# thread2 = Thread(target=fun)
+# thread3 = Thread(target=fun)
+# thread4 = Thread(target=fun)
+# # fun()
+# # fun()
+# thread1.start()
+# thread2.start()
+# thread3.start()
+# thread4.start()
+#
+# thread1.join()
+# thread2.join()
+# thread3.join()
+# thread4.join()
 
-thread1.join()
-thread2.join()
-thread3.join()
-thread4.join()
+for i in range(1, 100001):
+    t = Thread(target=fun, args=(i,))
+    t.start()
+    threads.append(t)
+
+for t in threads:
+    t.join()
 
 end = time.perf_counter()
 
